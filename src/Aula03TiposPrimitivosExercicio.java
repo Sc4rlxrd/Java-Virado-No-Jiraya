db6e716a8fc8eb0256babbda955c925c93bfd9ef
Sc4lxrd <guilhermesantos2004gss@gmail.com>
2024-07-10 06:55:50 -0300

    feat: atividade basica

---
+++ src/Aula03TiposPrimitivosExercicio.java
@@ -0,0 +1,23 @@
+/*
+       CRIE VARIAVEIS PARA OS CAMPOS DESCRITOS ABAIXO ENTRE <> E
+       IMPRIME A SEGUINTE MENSAGEM
+       EU <NOME>, MORANDO NO ENDEREÇO <ENDEREÇO>, CONFIRMO QUE RECEBI
+       O SALÁRIO DE <SALARIO>, NA DATA <DATA>
+
+       variaveis -> nome,endereço, salario, data
+
+   */
+
+public class Aula03TiposPrimitivosExercicio {
+
+    public static void main(String[] args) {
+
+        String nome = "Scarlxrd";
+        String endereco = "Rua catapimbas 158";
+        double salario = 4500;
+        int data= 1007;
+
+        System.out.println("Eu " + nome+ ", morando no endereço:  "+ endereco+ ", confirmo que recebi o salário de "+ salario + ", na data " + data);
+
+    }
+}
