3e8c6cc427345b75e2fcf813667f857c0834cf42
Sc4rlxrd <guilhermesantos2004gss@gmail.com>
2024-08-18 18:21:36 -0300

    feat: wrappers pt2 => introduction to autoboxing and unboxing

--- src/javacore/wrappers/test/WrapperTest01.java
+++ src/javacore/wrappers/test/WrapperTest01.java
@@ -4,15 +4,29 @@ public class WrapperTest01 {
     public static void main(String[] args) {
         //wrapper é um obj que  encapsular um tipo primitivo para se torna um obj
         // wrapper trabalha usando os conceitos do  polimorfismo, exemplo um wrapper long não vai aceitar um valor int por nao ser mais um tipo primitivo mas sim obj
+        // pt2: autoboxing transformar um tipo primitivo em wrapper já unboxing é o contrário é transformar um wrapper em primitivo
 
         Byte byteW =1;
         Short shortW=1;
-        Integer integerW=1;
+        Integer integerW=1; // autoboxing
         Long longW=10L;
         Float floatW=10F;
         Double doubleW=10D;
         Character charW='W';
         Boolean booleanW = false;
+
+        int i = integerW; // unboxing
+        Integer intW2 = Integer.parseInt("1");
+        boolean verdadeiro = Boolean.parseBoolean("TruE");
+
+        System.out.println(Character.isDigit('A'));
+        System.out.println(Character.isDigit('9'));
+        System.out.println(Character.isLetterOrDigit('!'));
+        System.out.println(Character.isUpperCase('A'));
+        System.out.println(Character.isLowerCase('a'));
+        System.out.println(Character.toUpperCase('a'));
+        System.out.println(Character.toLowerCase('A'));
+
 
     }
 }
