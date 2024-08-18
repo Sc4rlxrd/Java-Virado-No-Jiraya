4f09c5491ba5f59f98143089e785116399bd4967
Sc4rlxrd <guilhermesantos2004gss@gmail.com>
2024-08-18 18:01:49 -0300

    feat: introduction to wrappers pt1

---
+++ src/javacore/wrappers/test/WrapperTest01.java
@@ -0,0 +1,18 @@
+package javacore.wrappers.test;
+
+public class WrapperTest01 {
+    public static void main(String[] args) {
+        //wrapper é um obj que  encapsular um tipo primitivo para se torna um obj
+        // wrapper trabalha usando os conceitos do  polimorfismo, exemplo um wrapper long não vai aceitar um valor int por nao ser mais um tipo primitivo mas sim obj
+
+        Byte byteW =1;
+        Short shortW=1;
+        Integer integerW=1;
+        Long longW=10L;
+        Float floatW=10F;
+        Double doubleW=10D;
+        Character charW='W';
+        Boolean booleanW = false;
+
+    }
+}
