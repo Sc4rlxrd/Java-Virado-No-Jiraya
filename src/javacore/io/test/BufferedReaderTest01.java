8faf958dfe2eb45258eb170f469ad68e2b4a5c0b
Guilherme dos Santos <guilhermesantos2004gss@gmail.com>
2025-04-22 16:10:55 -0300

    feat: Introduction to the BufferedReader class and how it works under the hood

---
+++ src/javacore/io/test/BufferedReaderTest01.java
@@ -0,0 +1,29 @@
+package javacore.io.test;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+// File -> ok
+// FileWrite -> ok
+// FileReader -> ok
+// BufferedWrite -> ok
+// BufferedReader -> ok
+public class BufferedReaderTest01 {
+    public static void main(String[] args) {
+
+        File file = new File("file.txt");
+        try(FileReader fr = new FileReader(file);
+            BufferedReader br = new BufferedReader(fr)
+        ){
+            String linha;
+            while ((linha = br.readLine())!=null){
+                System.out.println(linha);
+            }
+
+
+        }catch (Exception e){
+            System.out.println("Exception: " + e);
+        }
+    }
+}
