0e80ca92a4ff447f19e22dd74211398659e77852
Guilherme dos Santos <guilhermesantos2004gss@gmail.com>
2025-04-23 14:52:52 -0300

    feat: Trying to normalize the paths and solve any problems that may appear.

---
+++ src/javacore/nio/test/PathTest03.java
@@ -0,0 +1,24 @@
+package javacore.nio.test;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class PathTest03 {
+    public static void main(String[] args) {
+        Path dir = Paths.get("home/scarlxrd");
+        Path arquivo = Paths.get("dev/arquivo.txt");
+        Path resolve = dir.resolve(arquivo);
+        System.out.println(resolve);
+
+        Path absolute = Paths.get("/home/scarlxrd");
+        Path relative = Paths.get("dev");
+        Path file = Paths.get("file.txt");
+
+        System.out.println(STR."1: \{absolute.resolve(relative)}");
+        System.out.println(STR."2: \{absolute.resolve(file)}");
+        System.out.println(STR."3: \{relative.resolve(absolute)}");
+        System.out.println(STR."4: \{relative.resolve(file)}");
+        System.out.println(STR."5: \{file.resolve(absolute)}");
+        System.out.println(STR."6: \{file.resolve(relative)}");
+    }
+}
