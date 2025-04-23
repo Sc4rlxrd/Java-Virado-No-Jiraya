7621064e9935aab568d2959f1b778fc8a8f3c335
Guilherme dos Santos <guilhermesantos2004gss@gmail.com>
2025-04-23 15:02:33 -0300

    refactor: rename file.

--- src/javacore/nio/test/ResolveTest01.java
+++ src/javacore/nio/test/ResolveTest01.java
@@ -3,7 +3,7 @@ package javacore.nio.test;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-public class PathTest03 {
+public class ResolveTest01 {
     public static void main(String[] args) {
         Path dir = Paths.get("home/scarlxrd");
         Path arquivo = Paths.get("dev/arquivo.txt");
