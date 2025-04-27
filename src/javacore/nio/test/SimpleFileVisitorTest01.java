5e99e9985e912509a43beea9a7b2203191d65bfe
Guilherme dos Santos <guilhermesantos2004gss@gmail.com>
2025-04-26 21:01:06 -0300

    refactor: conflict with class names.

--- src/javacore/nio/test/SimpleFileVisitorTest01.java
+++ src/javacore/nio/test/SimpleFileVisitorTest01.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
 
-class ListAllFiles extends SimpleFileVisitor<Path>{
+class ListAllFilesJava extends SimpleFileVisitor<Path>{
 
     @Override
     public FileVisitResult visitFile (Path file, BasicFileAttributes attrs){
@@ -20,7 +20,7 @@ class ListAllFiles extends SimpleFileVisitor<Path>{
 public class SimpleFileVisitorTest01 {
     public static void main(String[] args) throws IOException {
         Path root = Paths.get(".");
-        Files.walkFileTree(root, new ListAllFiles());
+        Files.walkFileTree(root, new ListAllFilesJava());
     }
 
 }
