a77b506faa89484ad6dcbdb2f34608bb20189a70
Guilherme dos Santos <guilhermesantos2004gss@gmail.com>
2025-05-10 15:58:13 -0300

    feat: Using List to store objects and access them in the loop.

---
+++ src/javacore/colecoes/test/SmartphoneListTest01.java
@@ -0,0 +1,33 @@
+package javacore.colecoes.test;
+
+import javacore.colecoes.dominio.Smartphone;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SmartphoneListTest01 {
+    public static void main(String[] args) {
+        Smartphone s1 = new Smartphone("1ABC1","Iphone");
+        Smartphone s2 = new Smartphone("22222","Pixel 8XL");
+        Smartphone s3 = new Smartphone("33333","Samsung");
+        List<Smartphone> smartphones = new ArrayList<>();
+        smartphones.add(s1);
+        smartphones.add(s2);
+        smartphones.add(0,s3);
+
+        for (Smartphone smartphone: smartphones){
+            System.out.println(smartphone);
+        }
+        Smartphone s4 = new Smartphone("2222233", "Pixel");
+        System.out.println(smartphones.contains(s4));
+        if (!smartphones.contains(s4)){
+            smartphones.add(s4);
+            System.out.println("Depois de add s4");
+            for(Smartphone smartphone: smartphones){
+                System.out.println(STR."Smartphones: \{smartphone}");
+            }
+        }
+
+
+    }
+}
