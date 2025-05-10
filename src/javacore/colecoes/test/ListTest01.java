e4c2506fe9f424d7f324a8449ea32e7f9a756709
Guilherme dos Santos <guilhermesantos2004gss@gmail.com>
2025-05-10 15:54:38 -0300

    feat: Introduction to list concept in Java collection class.

---
+++ src/javacore/colecoes/test/ListTest01.java
@@ -0,0 +1,49 @@
+package javacore.colecoes.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListTest01 {
+    public static void main(String[] args) {
+        List<String> nomes = new ArrayList<>(); //  1.5  implementado no java.
+        nomes.add("Guilherme");
+        nomes.add("Linux Manjaro");
+        nomes.remove(1);
+        nomes.add("Salve");
+        nomes.add("Salve");
+        nomes.add("Salve");
+        System.out.println(nomes.size());
+
+        System.out.println("Usando foreach");
+        for(String nome:nomes){
+            System.out.println(STR."Nomes:\{nome}");
+        }
+        System.out.println("-----------------------");
+        System.out.println("Usando for com índice");
+        for(int i = 0; i < nomes.size(); i++) {
+            System.out.println(STR."Nomes: \{nomes.get(i)}");
+        }
+        /*
+            se precisar criar uma lista de tipo primitivo vai ter que usar os Wrappers do java
+            Ex: List<Integer> numeros = new ArrayList<>(); => para lista com valores int
+            Ex: List<Long> numeros = new ArrayList<>();   => para lista com valores long
+            Ex: List<Char> numeros = new ArrayList<>();   => para lista com valores chars
+         */
+        System.out.println("-----------------------------");
+        System.out.println("Lista de numeros Int");
+        List<Integer> numeros1 = new ArrayList<>();
+        List<Integer> numeros2 = new ArrayList<>();
+        numeros1.add(11);
+        numeros1.add(12);
+        numeros1.add(13);
+        // para add os valores da lista 2 na 1 usar o addAll()
+        // as duas listas deve ser o mesmo tipo
+        numeros2.add(14);
+        numeros2.add(15);
+        numeros2.add(16);
+        numeros1.addAll(numeros2);
+        for(Integer number:numeros1){
+            System.out.println("Numeros Juntos: "+ number);
+        }
+    }
+}
