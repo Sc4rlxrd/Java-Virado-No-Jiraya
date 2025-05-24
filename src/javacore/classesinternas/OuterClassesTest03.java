eac45ac694e7e2d0f3139f67a16d083f5f0415b7
Guilherme dos Santos <guilhermesantos2004gss@gmail.com>
2025-05-24 17:48:40 -0300

    feat: Introduction to static nested classes and how it works.

---
+++ src/javacore/classesinternas/OuterClassesTest03.java
@@ -0,0 +1,21 @@
+package javacore.classesinternas;
+
+public class OuterClassesTest03 {
+   private String name = "Boruto";
+   // com o modificador estático somente posso acessar conteúdos estáticos.
+   static class Nested{
+       private String lastName = "Uzumaki";
+        void print(){
+
+            // assim eu consigo acessar o valor na variavel name
+            System.out.println(STR."\{new OuterClassesTest03().name} \{lastName}");
+        }
+   }
+
+    public static void main(String[] args) {
+        //Agora não preciso referencia a classe super, basta criar um obj padrão como no exemplo abaixo.
+        Nested nested = new Nested();
+        nested.print();
+
+    }
+}
