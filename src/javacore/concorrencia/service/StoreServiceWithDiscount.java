6c6a36b5adddab9a6e70a3cd4ef2efc58f004fe3
Guilherme dos Santos <guilhermesantos2004gss@gmail.com>
2025-07-25 23:09:08 -0300

    feat: I added a method that does async.

--- src/javacore/concorrencia/service/StoreServiceWithDiscount.java
+++ src/javacore/concorrencia/service/StoreServiceWithDiscount.java
@@ -20,7 +20,7 @@ public class StoreServiceWithDiscount {
     public String applyDiscount(Quote quote) {
         delay();
         double discountValue = quote.getPrice() * (100 - quote.getDiscountCode().getPercentage()) / 100;
-        return String.format("'%s' original price: '%2f'. Applying discount code '%s'. Final price: '%2f'",
+        return String.format("'%s' original price: '%.2f'. Applying discount code '%s'. Final price: '%.2f'",
                 quote.getStore(),
                 quote.getPrice(),
                 quote.getDiscountCode(),
