2b8c9023dfd5ae953c9d0e5a1aae2a75268de613
Guilherme dos Santos <guilhermesantos2004gss@gmail.com>
2025-08-02 18:14:52 -0300

    feat: A way to use lazy in a thread safe way using enumeration.

---
+++ src/javacore/padroesdeprojetos/dominio/AircraftSingletonEnum.java
@@ -0,0 +1,17 @@
+package javacore.padroesdeprojetos.dominio;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public enum AircraftSingletonEnum {
+    INSTANCE;
+    private final Set<String> availableSeats;
+    AircraftSingletonEnum(){
+        this.availableSeats = new HashSet<>();
+        this.availableSeats.add("1A");
+        this.availableSeats.add("1B");
+    }
+    public boolean bookSeat(String seat){
+        return availableSeats.remove(seat);
+    }
+}
