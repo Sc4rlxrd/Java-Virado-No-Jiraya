6597fa435044d1297345aaaab41e522984a9c2f1
Guilherme dos Santos <guilhermesantos2004gss@gmail.com>
2025-08-10 17:50:41 -0300

    feat: Creating a class to connect to MySQL using java.sql

---
+++ src/javacore/jdbc/conn/ConnectionFactory.java
@@ -0,0 +1,24 @@
+package javacore.jdbc.conn;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+    // java.sql -> Connection , Statement , ResultSet , DriverManager
+    public static Connection getConnection(){
+        String url = "jdbc:mysql://localhost:3306/anime_store";
+        String username = "root";
+        String password = "root";
+        try {
+            Connection connection = DriverManager.getConnection(url, username, password);
+            System.out.println(connection);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return null;
+
+    }
+
+
+}
---
+++ src/javacore/jdbc/test/ConnectionFactoryTest.java
@@ -0,0 +1,10 @@
+package javacore.jdbc.test;
+
+import javacore.jdbc.conn.ConnectionFactory;
+
+public class ConnectionFactoryTest {
+    public static void main(String[] args) {
+        ConnectionFactory.getConnection();
+
+    }
+}
