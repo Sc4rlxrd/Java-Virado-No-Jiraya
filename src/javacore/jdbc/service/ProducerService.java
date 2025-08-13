4b03a6a2260aac0da167bfffc8430ff6e55f1e23
Guilherme dos Santos <guilhermesantos2004gss@gmail.com>
2025-08-12 22:24:10 -0300

    feat: Learning how to update the database.

--- src/javacore/jdbc/service/ProducerService.java
+++ src/javacore/jdbc/service/ProducerService.java
@@ -7,10 +7,18 @@ public class ProducerService {
     public static  void save(Producer producer){
         ProducerRepository.save(producer);
     }
-    public static void delete(int id){
-        if (id<=0){
-            throw new IllegalArgumentException("Invalid value for id");
-        }
+    public static void delete(Integer id){
+        requireValid(id);
         ProducerRepository.delete(id);
+    }
+    public static  void update(Producer producer){
+        requireValid(producer.getId());
+        ProducerRepository.update(producer);
+    }
+    private static void requireValid(Integer id){
+        if (id == null || id<=0){
+            throw new IllegalArgumentException("Invalid value for id");
+
+        }
     }
 }
