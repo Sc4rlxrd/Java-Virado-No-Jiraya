fea812775f379c63a9ba9efe02fd4c69c62d74d0
Guilherme dos Santos <guilhermesantos2004gss@gmail.com>
2025-08-23 21:55:14 -0300

    feat: creation of the findById method.

--- src/javacore/crud/repository/ProducerRepository.java
+++ src/javacore/crud/repository/ProducerRepository.java
@@ -7,6 +7,7 @@ import lombok.extern.log4j.Log4j2;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Log4j2
 public class ProducerRepository {
@@ -61,6 +62,25 @@ public class ProducerRepository {
         String sql = "INSERT INTO `anime_store`.`producer` (`name`) VALUES (?);";
         PreparedStatement ps = conn.prepareStatement(sql);
         ps.setString(1, producer.getName());
+        return ps;
+    }
+
+    public static Optional<Producer> findById(Integer id) {
+        log.info("looking for producer id");
+        try (Connection conn = javacore.jdbc.conn.ConnectionFactory.getConnection();
+             PreparedStatement ps = PreparedStatementFindById(conn,id)) {
+            ResultSet rs = ps.executeQuery();
+            if (!rs.next()) return Optional.empty();
+            return Optional.of(Producer.builder().id(rs.getInt("id")).name(rs.getString("name")).build());
+        } catch (SQLException e) {
+            log.error("Error while trying to insert producer '{}'", id, e);
+        }
+        return Optional.empty();
+    }
+    private static PreparedStatement PreparedStatementFindById(Connection conn, Integer id) throws SQLException {
+        String sql = "SELECT *  FROM `anime_store`. `producer` WHERE (`id` = ?);";
+        PreparedStatement ps = conn.prepareStatement(sql);
+        ps.setInt(1, id);
         return ps;
     }
 }
